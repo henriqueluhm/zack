2f2e3784219a46359be0e219f7325e8cd1c7e453
Henrique Luhm <62779084+henriqueluhm@users.noreply.github.com>
2025-06-27 00:34:09 -0300

    refactor: using cursor module and new mode type

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -1,24 +1,21 @@
-use crate::app::modes::EditorMode;
+use crate::app::cursor::Cursor;
+use crate::app::modes::normal::NormalMode;
+use crate::app::modes::{Mode, change_mode};
 use crate::event::{AppEvent, Event, EventHandler};
-use crossterm::{QueueableCommand, cursor};
+use ratatui::DefaultTerminal;
 use ratatui::Frame;
-use ratatui::layout::Rect;
-use ratatui::{
-    DefaultTerminal,
-    crossterm::event::{KeyCode, KeyEvent, KeyModifiers},
-};
 use ropey::Rope;
-use std::io::{Write, stdout};
 
+pub mod cursor;
 pub mod modes;
 
 #[derive(Debug)]
 pub struct App {
     pub running: bool,
-    pub mode: EditorMode,
+    pub mode: Box<dyn Mode>,
+    pub cursor: Cursor,
     pub buffer: Rope,
-    pub cursor: (usize, usize),
-    pub events: EventHandler,
+    pub event_handler: EventHandler,
 }
 
 impl Default for App {
@@ -26,9 +23,9 @@ impl Default for App {
         Self {
             running: true,
             buffer: Rope::from_str("Hello, Zack!"),
-            mode: EditorMode::Normal,
-            cursor: (0, 0),
-            events: EventHandler::new(),
+            mode: Box::new(NormalMode),
+            cursor: Cursor::new(),
+            event_handler: EventHandler::new(),
         }
     }
 }
@@ -40,140 +37,72 @@ impl App {
 
     pub fn run(mut self, mut terminal: DefaultTerminal) -> color_eyre::Result<()> {
         while self.running {
-            terminal.draw(|frame| self.render_with_cursor(frame))?;
-            self.handle_events()?;
+            terminal.draw(|frame| self.render(frame))?;
+            self.handle_event_handler()?;
         }
 
         Ok(())
     }
 
-    pub fn render_with_cursor(&self, frame: &mut Frame) {
-        let area = frame.area();
+    pub fn render(&self, frame: &mut Frame) {
+        frame.render_widget(self, frame.area());
 
-        frame.render_widget(self, area);
-
-        let cursor_position = self.calculate_cursor_position(area);
-
-        let mut stdout = stdout();
-
-        frame.set_cursor_position(cursor_position);
-
-        if self.mode == EditorMode::Insert {
-            stdout.queue(cursor::SetCursorStyle::SteadyBar).unwrap();
-        } else {
-            stdout.queue(cursor::SetCursorStyle::SteadyBlock).unwrap();
-        }
-
-        stdout.flush().unwrap();
+        self.cursor
+            .render_cursor(frame, self.mode.get_current_mode())
     }
 
-    fn calculate_cursor_position(&self, area: Rect) -> ratatui::layout::Position {
-        let text_area = Rect {
-            x: area.x + 1,
-            y: area.y + 1,
-            width: area.width.saturating_sub(2),
-            height: area.height.saturating_sub(2),
-        };
-
-        let (cursor_line, cursor_col) = self.cursor;
-
-        let clamped_line = cursor_line.min(text_area.height.saturating_sub(1) as usize);
-        let clamped_col = cursor_col.min(text_area.width.saturating_sub(1) as usize);
-
-        ratatui::layout::Position {
-            x: text_area.x + clamped_col as u16,
-            y: text_area.y + clamped_line as u16,
-        }
-    }
-
-    pub fn handle_events(&mut self) -> color_eyre::Result<()> {
-        match self.events.next()? {
+    pub fn handle_event_handler(&mut self) -> color_eyre::Result<()> {
+        match self.event_handler.next()? {
             Event::Tick => self.tick(),
-            Event::Crossterm(event) => {
-                if let crossterm::event::Event::Key(key_event) = event {
-                    self.handle_key_event(key_event)?
-                }
+            Event::Crossterm(event) => self.handle_crossterm_event(event),
+            Event::App(event) => self.handle_app_event(event),
+        }
+
+        Ok(())
+    }
+
+    pub fn handle_crossterm_event(&mut self, event: crossterm::event::Event) {
+        if let crossterm::event::Event::Key(key_event) = event {
+            for event in self.mode.handle_key(key_event) {
+                self.event_handler.send(event);
             }
-            Event::App(app_event) => match app_event {
-                AppEvent::InsertChar(char) => {
-                    let (line, col) = self.cursor;
+        }
+    }
+
+    pub fn handle_app_event(&mut self, event: AppEvent) {
+        match event {
+            AppEvent::ChangeToMode(new_mode) => change_mode(new_mode, self),
+            AppEvent::InsertChar(char) => {
+                let (line, col) = self.cursor.position;
+                let char_index = self.calculate_char_index(line, col);
+                self.buffer.insert_char(char_index, char);
+                self.cursor.position.1 += 1;
+            }
+            AppEvent::DeleteChar => {
+                let (line, col) = self.cursor.position;
+                if col > 0 {
                     let char_index = self.calculate_char_index(line, col);
-                    self.buffer.insert_char(char_index, char);
-                    self.cursor.1 += 1;
+                    self.buffer.remove(char_index - 1..char_index);
+                    self.cursor.position.1 -= 1;
                 }
-                AppEvent::DeleteChar => {
-                    let (line, col) = self.cursor;
-                    if col > 0 {
-                        let char_index = self.calculate_char_index(line, col);
-                        self.buffer.remove(char_index - 1..char_index);
-                        self.cursor.1 -= 1;
-                    }
-                }
-                AppEvent::MoveCursorLeft => {
-                    if self.cursor.1 > 0 {
-                        self.cursor.1 -= 1;
-                    }
-                }
-                AppEvent::MoveCursorRight => {
-                    self.cursor.1 += 1;
-                }
-                AppEvent::MoveCursorUp => {
-                    if self.cursor.0 > 0 {
-                        self.cursor.0 -= 1;
-                    }
-                }
-                AppEvent::MoveCursorDown => {
-                    self.cursor.0 += 1;
-                }
-                AppEvent::Quit => self.quit(),
-            },
-        }
-
-        Ok(())
-    }
-
-    pub fn handle_key_event(&mut self, key_event: KeyEvent) -> color_eyre::Result<()> {
-        use EditorMode::*;
-
-        match self.mode {
-            Insert => self.handle_insert_mode(key_event),
-            Normal => self.handle_normal_mode(key_event),
-        }
-
-        Ok(())
-    }
-
-    fn handle_insert_mode(&mut self, key_event: KeyEvent) {
-        match key_event.code {
-            KeyCode::Esc => self.mode = EditorMode::Normal,
-            KeyCode::Backspace => self.events.send(AppEvent::DeleteChar),
-            KeyCode::Char(c) => self.events.send(AppEvent::InsertChar(c)),
-            KeyCode::Left => self.events.send(AppEvent::MoveCursorLeft),
-            KeyCode::Right => self.events.send(AppEvent::MoveCursorRight),
-            KeyCode::Up => self.events.send(AppEvent::MoveCursorUp),
-            KeyCode::Down => self.events.send(AppEvent::MoveCursorDown),
-            _ => {}
-        }
-    }
-
-    fn handle_normal_mode(&mut self, key_event: KeyEvent) {
-        match key_event.code {
-            KeyCode::Char('i') => self.mode = EditorMode::Insert,
-            KeyCode::Char('a') => {
-                self.events.send(AppEvent::MoveCursorRight);
-                self.mode = EditorMode::Insert
             }
-            KeyCode::Char('h') => self.events.send(AppEvent::MoveCursorLeft),
-            KeyCode::Char('l') => self.events.send(AppEvent::MoveCursorRight),
-            KeyCode::Char('j') => self.events.send(AppEvent::MoveCursorDown),
-            KeyCode::Char('k') => self.events.send(AppEvent::MoveCursorUp),
-            KeyCode::Char('q') | KeyCode::Esc => self.events.send(AppEvent::Quit),
-
-            KeyCode::Char('c' | 'C') if key_event.modifiers.contains(KeyModifiers::CONTROL) => {
-                self.events.send(AppEvent::Quit);
+            AppEvent::MoveCursorLeft => {
+                if self.cursor.position.1 > 0 {
+                    self.cursor.position.1 -= 1;
+                }
             }
-
-            _ => {}
+            AppEvent::MoveCursorRight => {
+                self.cursor.position.1 += 1;
+            }
+            AppEvent::MoveCursorUp => {
+                if self.cursor.position.0 > 0 {
+                    self.cursor.position.0 -= 1;
+                }
+            }
+            AppEvent::MoveCursorDown => {
+                self.cursor.position.0 += 1;
+            }
+            AppEvent::Quit => self.quit(),
         }
     }
 
