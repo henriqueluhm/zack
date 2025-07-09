cfac5c73daec970677b2a6b70a7e95cfcd1d2368
Henrique Luhm <62779084+henriqueluhm@users.noreply.github.com>
2025-07-08 22:09:36 -0300

    chore: setup logging in debug mode

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,22 @@ mod ui;
 
 use app::App;
 
+#[cfg(feature = "debug-logs")]
+fn init_logging() {
+    use simplelog::*;
+    let _ = CombinedLogger::init(vec![WriteLogger::new(
+        LevelFilter::Debug,
+        Config::default(),
+        std::fs::File::create("zack.log").unwrap(),
+    )]);
+}
+
+#[cfg(not(feature = "debug-logs"))]
+fn init_logging() {}
+
 fn main() -> color_eyre::Result<()> {
     color_eyre::install()?;
+    init_logging();
 
     let terminal = ratatui::init();
     let result = App::new().run(terminal);
